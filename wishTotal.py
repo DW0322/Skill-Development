41a797d7e451fdf5461a96ebd0cbff640fb1575d
DW0322 <123480673+DW0322@users.noreply.github.com>
2024-11-19 23:46:10 +0000

    Update wishTotal.py
    Simplified process:
    
    - Doesn't open each item page and search that page for the price
    - Searches the wishlist page and gathers all available prices form there.

--- wishTotal.py
+++ wishTotal.py
@@ -1,35 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 
 def priceChecker():
+
     url = input('Please provide the link to your amazon wishlist: ')
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
+
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
+    prices = soup.find_all('span', class_='a-offscreen')
 
-    base_url = "https://www.amazon.co.uk" #change this to your countries corresponding url
-
-    links = []
-    for link in soup.find_all('a', href=True):
-        href = link['href']
-        if 'dp_it_im' in href:
-            links.append(base_url + href)
-
-    total = 0
-    for link in links:
-        time.sleep(2)  # Rate-limiting to avoid scraper limiting
-        response = requests.get(link, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        price_text = soup.find('span', class_='a-offscreen')
-        if price_text:
-            try:
-                price_float = float(price_text.text.replace('£', '').replace(',', '')) #change £ sign to your countries corresponding url
-                total += price_float
-            except ValueError:
-                print(f"Error parsing price for {link}")
-    print(f"Total: {total}")
+    cleaned_prices = []
+    for price in prices:
+        cleaned_price = price.string
+        cleaned_prices.append(float(cleaned_price[1:]))
+        
+    print(sum(cleaned_prices))
 
 priceChecker()
